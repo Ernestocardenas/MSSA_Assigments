49b470dc3df3a43fd681e766dafce0881de43650
Ernestocardenas <ecard054@fiu.edu>
2022-10-25 16:43:49 -0400

    Assigment-4.2  initial upload

---
+++ Assigment-4.2/Assigment-4.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        TwoDArray();
+    }
+
+    public static void TwoDArray()
+    {
+        int i, j;
+        int[,] array = new int[3, 2];
+
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 2; j++)
+            {
+                Console.Write("element - [{0},{1}] : ", i, j);
+                array[i,j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("The matix is: \n");
+        for(i=0; i < 3; i++)
+        {
+            Console.Write("\n");
+            for(j=0; j < 2; j++)
+                Console.Write("|{0}",array[i,j]);
+        }
+        Console.Write("\n\n");
+    }
+
+
+
+}
